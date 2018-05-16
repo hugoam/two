4cbce26a45ca463698a1c7048b9d45d483a1f732
Hugo Amnov <hugo.amiard@laposte.net>
2018-05-17 01:42:55 +0200

    Update year

--- src/obj/Memory/Pool.h
+++ src/obj/Memory/Pool.h
@@ -1,4 +1,4 @@
-//  Copyright (c) 2016 Hugo Amiard hugo.amiard@laposte.net
+//  Copyright (c) 2018 Hugo Amiard hugo.amiard@laposte.net
 //  This software is provided 'as-is' under the zlib License, see the LICENSE.txt file.
 //  This notice and the license may not be removed or altered from any source distribution.
 //  Copyright (c) 2015 Hugo Amiard hugo.amiard@laposte.net
