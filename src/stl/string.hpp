817de3aa25ddafcad46b1fcfb91ad01719626542
Hugo Amnov <hugo.amiard@laposte.net>
2019-02-04 13:58:51 +0100

    Cleanup

--- src/stl/string.hpp
+++ src/stl/string.hpp
@@ -16,8 +16,8 @@ namespace stl {
 	template <class Alloc>
 	inline basic_string<Alloc>::basic_string()
 	{
-		m_first = m_last = m_small;
-		m_capacity = m_small + c_nbuffer;
+		this->m_first = this->m_last = m_small;
+		this->m_capacity = m_small + c_nbuffer;
 	}
 
 	template <class Alloc>
@@ -64,7 +64,7 @@ namespace stl {
 		: basic_string()
 	{
 		this->reserve(len);
-		fill_urange(m_first, m_last, c);
+		fill_urange(this->m_first, this->m_last, c);
 	}
 
 	template <class Alloc>
@@ -82,29 +82,29 @@ namespace stl {
 
 	template <class Alloc>
 	inline basic_string<Alloc>::~basic_string() {
-		if(m_first == m_small)
-			m_first = m_last = m_capacity = nullptr;
+		if(this->m_first == m_small)
+			this->m_first = this->m_last = this->m_capacity = nullptr;
 	}
 
 	template <class Alloc>
 	inline const char* basic_string<Alloc>::c_str() const {
-		return m_first;
+		return this->m_first;
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::reset(size_t size) {
-		m_first = m_small;
-		m_last = m_small + size;
-		m_capacity = m_small + c_nbuffer;
+		this->m_first = m_small;
+		this->m_last = m_small + size;
+		this->m_capacity = m_small + c_nbuffer;
 		m_small[size] = 0;
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::fill(const char* first, const char* last)
 	{
-		copy_urange(m_last, first, last);
-		m_last += last - first;
-		*m_last = 0;
+		copy_urange(this->m_last, first, last);
+		this->m_last += last - first;
+		*this->m_last = 0;
 	}
 
 	template <class Alloc>
@@ -122,41 +122,41 @@ namespace stl {
 	template <class Alloc>
 	inline void basic_string<Alloc>::resize(size_t size) {
 		this->reserve(size);
-		fill_urange(m_last, m_first + size + 1);
-		m_last = m_first + size;
-		*m_last = 0;
+		fill_urange(this->m_last, this->m_first + size + 1);
+		this->m_last = this->m_first + size;
+		*this->m_last = 0;
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::resize(size_t size, char value) {
 		this->reserve(size);
-		fill_urange(m_last, m_first + size + 1, value);
-		m_last = m_first + size;
-		*m_last = 0;
+		fill_urange(this->m_last, this->m_first + size + 1, value);
+		this->m_last = this->m_first + size;
+		*this->m_last = 0;
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::clear() {
-		m_last = m_first;
-		*m_last = 0;
+		this->m_last = this->m_first;
+		*this->m_last = 0;
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::reserve(size_t capacity) {
-		buffer<char, Alloc, 1>::reserve(capacity, m_first == m_small);
+		buffer<char, Alloc, 1>::reserve(capacity, this->m_first == m_small);
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::push_back(char c) {
-		this->grow(this->size() + 1, m_first == m_small);
-		*m_last++ = c;
-		*m_last = 0;
+		this->grow(this->size() + 1, this->m_first == m_small);
+		*this->m_last++ = c;
+		*this->m_last = 0;
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::pop_back() {
-		m_last--;
-		*m_last = 0;
+		this->m_last--;
+		*this->m_last = 0;
 	}
 
 	template <class Alloc>
@@ -165,8 +165,8 @@ namespace stl {
 
 		char tbuffer[c_nbuffer];
 
-		if(m_first == other.m_small)
-			for(pointer it = other.m_small, end = m_last, out = tbuffer; it != end; ++it, ++out)
+		if(this->m_first == other.m_small)
+			for(pointer it = other.m_small, end = this->m_last, out = tbuffer; it != end; ++it, ++out)
 				*out = *it;
 
 		if(other.m_first == m_small) {
@@ -176,27 +176,27 @@ namespace stl {
 			*other.m_last = 0;
 		}
 
-		if(m_first == other.m_small) {
+		if(this->m_first == other.m_small) {
 			this->reset(this->size());
-			for(pointer it = m_first, end = m_last, in = tbuffer; it != end; ++it, ++in)
+			for(pointer it = this->m_first, end = this->m_last, in = tbuffer; it != end; ++it, ++in)
 				*it = *in;
-			*m_last = 0;
+			*this->m_last = 0;
 		}
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::shrink_to_fit() {
-		if(m_first != m_small)
+		if(this->m_first != m_small)
 			buffer<char, Alloc, 1>::shrink_to_fit();
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::append(const char* first, const char* last) {
 		const size_t newsize = size_t(this->size() + (last - first));
-		this->grow(newsize, m_first == m_small);
-		copy_urange(m_last, first, last);
-		m_last += last - first;
-		*m_last = 0;
+		this->grow(newsize, this->m_first == m_small);
+		copy_urange(this->m_last, first, last);
+		this->m_last += last - first;
+		*this->m_last = 0;
 	}
 
 	template <class Alloc>
@@ -218,17 +218,17 @@ namespace stl {
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::insert(size_t where, char value) {
-		this->insert(m_first + where, value);
+		this->insert(this->m_first + where, value);
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::insert(size_t where, const char* first, const char* last) {
-		this->insert(m_first + where, first, last);
+		this->insert(this->m_first + where, first, last);
 	}
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::insert(size_t where, const basic_string& other) {
-		this->insert(m_first + where, other.begin(), other.end());
+		this->insert(this->m_first + where, other.begin(), other.end());
 	}
 
 	template <class Alloc>
@@ -239,9 +239,9 @@ namespace stl {
 	template <class Alloc>
 	inline void basic_string<Alloc>::insert(iterator where, const char* first, const char* last) {
 		const size_t count = last - first;
-		where = this->spread(where, count, m_first == m_small);
+		where = this->spread(where, count, this->m_first == m_small);
 		copy_urange(where, first, last);
-		*m_last = 0;
+		*this->m_last = 0;
 	}
 
 	template <class Alloc>
@@ -257,7 +257,7 @@ namespace stl {
 
 	template <class Alloc>
 	inline void basic_string<Alloc>::erase(size_t pos, size_t len) {
-		this->erase(m_first + pos, len == npos ? m_last : m_first + pos + len);
+		this->erase(this->m_first + pos, len == npos ? this->m_last : this->m_first + pos + len);
 	}
 
 	template <class Alloc>
@@ -268,23 +268,23 @@ namespace stl {
 	template <class Alloc>
 	inline typename basic_string<Alloc>::iterator basic_string<Alloc>::erase(iterator first, iterator last) {
 		const size_t count = (last - first);
-		for (pointer it = last, end = m_last, dest = first; it != end; ++it, ++dest)
+		for (pointer it = last, end = this->m_last, dest = first; it != end; ++it, ++dest)
 			*dest = *it;
-		m_last -= count;
-		*m_last = 0;
+		this->m_last -= count;
+		*this->m_last = 0;
 		return first;
 	}
 
 	template <class Alloc>
 	inline basic_string<Alloc> basic_string<Alloc>::substr(size_t begin, size_t count) const {
-		return basic_string(m_first + begin, min(count, this->size() - begin));
+		return basic_string(this->m_first + begin, min(count, this->size() - begin));
 	}
 
 	template <class Alloc>
 	inline int basic_string<Alloc>::compare(const basic_string& other) const {
-		pointer first1 = m_first;
+		pointer first1 = this->m_first;
 		pointer first2 = other.m_first;
-		const pointer last1 = m_last;
+		const pointer last1 = this->m_last;
 		const pointer last2 = other.m_last;
 
 		const size_t count1 = size_t(last1 - first1);
@@ -330,7 +330,7 @@ namespace stl {
 		{
 			const char* match = string_find(hay + offset, hay_size - offset, c);
 			if(match)
-				return size_t(match - m_first);
+				return size_t(match - this->m_first);
 		}
 
 		return size_t(-1);
@@ -339,7 +339,7 @@ namespace stl {
 	template <class Alloc>
 	inline size_t basic_string<Alloc>::rfind(char c, const size_t offset) const {
 		const size_t hay_size = size();
-		const char* hay = m_first;
+		const char* hay = this->m_first;
 		if(hay_size != 0)
 			for(const char* current = hay + min(offset, hay_size - 1); current != hay; --current)
 			{
