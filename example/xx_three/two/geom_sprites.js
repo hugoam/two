b56a7292a1641a0d8b99467a626332d1766c00d9
Hugo Amnov <hugo.amiard@laposte.net>
2019-03-10 03:26:16 +0100

    WIP

--- example/xx_three/two/geom_sprites.js
+++ example/xx_three/two/geom_sprites.js
@@ -1,34 +1,36 @@
-var vertex_shader = `$input a_position, a_texcoord0, i_data0\n'
-    '$output v_texcoord0, v_scale\n'
-    '\n'
-    '#include <common.sh>\n'
-    '\n'
-    'void main()\n'
-    '{\n'
-    '   var i_position = i_data0.xyz;'
-    '	var timexyz = i_position + vec3_splat(u_time / 2.0);\n'
-    '	var scale =  Math.sin(timexyz.x * 2.1) + Math.sin(timexyz.y * 3.2) + Math.sin(timexyz.z * 4.3);\n'
-    '	v_scale = scale;\n'
-    '	var size = scale * 10.0 + 10.0;\n'
-    '	var view = mul(u_modelView, new two.vec4(i_position, 1.0)).xyz;\n'
-    '	view += a_position * size;\n'
-    '	v_texcoord0 = new two.vec4(a_texcoord0.xy, 0.0, 0.0);\n'
-    '	gl_Position = mul(u_proj, new two.vec4(view, 1.0));\n'
-    '\n'
-    '}`;
+// geom_sprites.js
 
-var fragment_shader = `$input v_texcoord0, v_scale\n'
-    '\n'
-    '#include <common.sh>\n'
-    '#include <convert.sh>\n'
-    '\n'
-    'void main()\n'
-    '{\n'
-    '	vec4 color = texture2D(s_color, v_texcoord0.xy);\n'
-    '   var hsl = hsl_to_rgb(new two.vec3(v_scale/5.0, 1.0, 0.5));\n'
-    '	gl_FragColor = new two.vec4(color.rgb * hsl.rgb, color.a);\n'
-    '	if (color.a < 0.5) discard;\n'
-    '}`;
+var vertex_shader = `$input a_position, a_texcoord0, i_data0
+    $output v_texcoord0, v_scale
+    
+    #include <common.sh>
+    
+    void main()
+    {
+        var i_position = i_data0.xyz;'
+        var timexyz = i_position + vec3_splat(u_time / 2.0);
+        var scale =  Math.sin(timexyz.x * 2.1) + Math.sin(timexyz.y * 3.2) + Math.sin(timexyz.z * 4.3);
+        v_scale = scale;
+        var size = scale * 10.0 + 10.0;
+        var view = mul(u_modelView, new two.vec4(i_position, 1.0)).xyz;
+        view += a_position * size;
+        v_texcoord0 = new two.vec4(a_texcoord0.xy, 0.0, 0.0);
+        gl_Position = mul(u_proj, new two.vec4(view, 1.0));
+    
+    }`;
+
+var fragment_shader = `$input v_texcoord0, v_scale
+    
+    #include <common.sh>
+    #include <convert.sh>
+    
+    void main()
+    {
+        vec4 color = texture2D(s_color, v_texcoord0.xy);
+        var hsl = hsl_to_rgb(new two.vec3(v_scale/5.0, 1.0, 0.5));
+        gl_FragColor = new two.vec4(color.rgb * hsl.rgb, color.a);
+        if (color.a < 0.5) discard;
+    }`;
 
 var particleCount = 75000;
 
@@ -39,9 +41,6 @@ var scene = viewer.scene;
 
 if(typeof this.state == 'undefined') {
     this.state = 1;
-
-    //struct Instance { var position; var distance; };
-    this.instances = new Float32Array(particleCount);
 
     this.node = nullptr;
     this.batch = nullptr;
@@ -71,6 +70,9 @@ if(typeof this.state == 'undefined') {
     //geometry.index = circleGeometry.index;
     //geometry.attributes = circleGeometry.attributes;
 
+    //struct Instance { var position; var distance; };
+    var instances = new Float32Array(particleCount);
+
     for(var i = 0; i < particleCount; ++i)
     {
         instances[i] = { new two.vec3(Math.random(), Math.random(), Math.random()) * 2.0 - 1.0 };
@@ -80,15 +82,14 @@ if(typeof this.state == 'undefined') {
     var it = scene.items().add(new two.Item(n, circleGeometry, 0, material));
     this.node = n;
 
-    batch = two.gfx.batches(scene).add(new two.Batch(it));
+    var batch = two.gfx.batches(scene).add(new two.Batch(it));
     it.batch = batch;
+    
+    batch.cache(instances, 4);
 }
 
-var time = app.gfx.time / 2.0;
+//var time = app.gfx.time / 2.0;
 
 var scale = new two.vec3(500.0);
 var angles = new two.vec3(time * 0.2, time * 0.4, 0.0);
-this.node.apply(scale, new two.quat(angles), new two.vec3(0.0));
-
-span<float> memory = batch->begin(instances.length, sizeof(Instance));
-memcpy(memory.data(), instances.data(), memory.length * sizeof(float));
+this.node.apply(new two.vec3(0.0), new two.quat(angles), scale);
