4da651ea0d7db742d5c9e2ea699b3b7c850f7393
Hugo Amnov <hugo.amiard@laposte.net>
2019-04-09 17:22:01 +0200

    Fix

--- example/xx_three/tiled_forward.cpp
+++ example/xx_three/tiled_forward.cpp
@@ -78,7 +78,7 @@ static string tiled_phong_fragment =
 	"{\n"
 	"#include <pbr/fs_fragment.sh>\n"
 	"#include <pbr/fs_phong_material.sh>\n"
-		"PhongMaterial material = matphong;"
+		"PhongMaterial material = matphong;\n"
 	"#include <pbr/fs_phong.sh>\n"
 	+ lights_fragment + "\n" +
 	"#include <pbr/fs_out_pbr.sh>\n"
